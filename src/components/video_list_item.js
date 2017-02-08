16fb0203c73d454ff6525087c8ef3c59856b838f
Omar Ceesay <Omar.Ceesay@bluecrossmn.com>
2017-02-08 11:52:46 -0600

    #31

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 const VideoListItem = (props) => {
-  const video = props.videos;
-  
+  const video = props.video;
+  const onVideoSelect = props.onVideoSelect;
+  console.log(video);
+  const imageUrl = video.snippet.thumbnails.default.url;
+
   return (
-    <li className="panel panel-primary">Video</li>
+    <li onClick={() => onVideoSelect(video)} className="list-group-item" style={{cursor: "pointer"}}>
+      <div className="media-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl}/>
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/SearchBar';
 import VideoList from "./components/video_list.js";
+import VideoDetail from "./components/video_detail.js";
 /*<VideoList videos={this.state.videos} /> PASSING DATA LIKE THIS IS CALLED PROPS*/
 
 const API_KEY = "AIzaSyBMzPmcl0Bc2vLa2O5a5Gn-843K-3x6BfM";
@@ -12,17 +13,24 @@ class App extends Component{
   constructor(props){
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
-      this.setState({videos: data});
+    YTSearch({key: API_KEY, term: 'react js'}, (data) => {
+      this.setState({
+        videos: data,
+        selectedVideo: data[0]
+      });
     });
   }
   render(){
     return(
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos} />
       </div>
     )
   }
